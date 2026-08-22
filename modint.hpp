7ca87083af70972f12a38faa7c5b7a0278ffe5eb
tau1235 <tausokky5321@gmail.com>
2026-08-23 05:13:38 +0900

    Create modint.hpp

---
+++ modint.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+template<int mod> struct modint{
+  int x;
+  modint():x(0){}
+  modint(long long x_){
+    x_%=mod;
+    if (x_<0) x_+=mod;
+    x=(int)x_;
+  }
+  int val()const{return x;}
+  modint& operator+=(const modint &r){
+    x+=r.x;
+    if (x>=mod) x-=mod;
+    return *this;
+  }
+  modint& operator-=(const modint &r){
+    x-=r.x;
+    if (x<0) x+=mod;
+    return *this;
+  }
+  modint& operator*=(const modint &r){
+    x=(int)((long long)x*r.x%mod);
+    return *this;
+  }
+  modint& operator/=(const modint &r){return *this*=r.inv();}
+  friend modint operator+(const modint &l,const modint &r){return modint(l)+=r;}
+  friend modint operator-(const modint &l,const modint &r){return modint(l)-=r;}
+  friend modint operator*(const modint &l,const modint &r){return modint(l)*=r;}
+  friend modint operator/(const modint &l,const modint &r){return modint(l)/=r;}
+  modint operator+()const{return *this;}
+  modint operator-()const{return modint()-*this;}
+  modint& operator++(){
+    if (++x==mod) x=0;
+    return *this;
+  }
+  modint& operator--(){
+    if (x--==0) x=mod-1;
+    return *this;
+  }
+  modint operator++(int){
+    modint ret=*this;
+    if (++x==mod) x=0;
+    return ret;
+  }
+  modint operator--(int){
+    modint ret=*this;
+    if (x--==0) x=mod-1;
+    return ret;
+  } 
+  friend bool operator==(const modint &l,const modint &r){return l.x==r.x;}
+  friend bool operator!=(const modint &l,const modint &r){return l.x!=r.x;}
+  modint inv()const{return this->pow(mod-2);}
+  modint pow(unsigned long long k)const{
+    modint ret=1,pw=*this;
+    while (k){
+      if (k&1) ret*=pw;
+      pw*=pw;
+      k>>=1;
+    }
+    return ret;
+  }
+  friend istream &operator>>(istream &is,modint &p){
+    long long x;
+    is>>x;
+    p=modint(x);
+    return is;
+  }
+  friend ostream &operator<<(ostream &os,const modint &p){return os<<p.x;}
+}; using mint=modint<998244353>;
