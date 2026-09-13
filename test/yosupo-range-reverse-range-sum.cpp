905afd752847b96889bc70fd31f16479457d56aa
tau1235 <tausokky5321@gmail.com>
2026-09-13 18:03:16 +0900

    Create yosupo-range-reverse-range-sum.cpp

---
+++ test/yosupo-range-reverse-range-sum.cpp
@@ -0,0 +1,33 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/range_reverse_range_sum
+
+#include<bits/stdc++.h>
+using namespace std;
+#include"splaytree.hpp"
+
+using ll=long long;
+ll op(ll a,ll b){return a+b;}
+ll e(){return 0;}
+ll mapping(ll f,ll x){return x;}
+ll composition(ll f,ll g){return 0;}
+ll id(){return 0;}
+
+int main(){
+  int n,q;
+  cin>>n>>q;
+  vector<ll> a(n);
+  SplayTree<ll,op,e,ll,mapping,composition,id> st;
+  for (int i=0;i<n;i++){
+    int a;
+    cin>>a;
+    st.insert(i,a);
+  }
+  while (q--){
+    int t,l,r;
+    cin>>t>>l>>r;
+    if (t==0) st.reverse(l,r);
+    if (t==1){
+      ll ans=st.prod(l,r);
+      cout<<ans<<endl;
+    }
+  }
+}
